5259f86bab08d5b7148454e53e488cd2d2d15872
ShumaOtsuka <66660624+ShumaOtsuka@users.noreply.github.com>
2022-11-06 15:43:58 +0900

    Add files via upload

---
+++ svm.py
@@ -0,0 +1,29 @@
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler 
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+input_train, input_test, output_train, output_test = train_test_split('''tfidfベクトルを導入''', '''ラベルを導入''', test_size=0.3, random_state=None )
+
+sc = StandardScaler()
+sc.fit(input_train)
+input_train_std = sc.transform(input_train)
+input_test_std = sc.transform(input_test)
+
+# 学習インスタンス生成
+svc_model = SVC(kernel='linear', random_state=None)
+
+# 学習
+svc_model.fit(input_train_std, output_train)
+
+#traning dataのaccuracy
+pred_train = svc_model.predict(input_train_std)
+accuracy_train = accuracy_score(output_train, pred_train)
+print('traning data accuracy： %.2f' % accuracy_train)
+
+#test dataのaccuracy
+pred_test = svc_model.predict(input_test_std)
+accuracy_test = accuracy_score(output_test, pred_test)
+print('test data accuracy： %.2f' % accuracy_test)
